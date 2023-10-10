d0873147352b2437780dd6f46d4b92b340fb9942
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2023-10-10 00:23:26 -0300

    docker compose for easy project init and usage

--- fox-web-api/Program.cs
+++ fox-web-api/Program.cs
@@ -7,6 +7,8 @@ namespace fox_web_api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 
             // Add services to the container.
 
@@ -15,18 +17,34 @@ namespace fox_web_api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Defaulconnection not provided"));
+                var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+                var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+                var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+                var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; User Id=sa;Password={dbPassword};TrustServerCertificate=true;";
+                options.UseSqlServer(connectionString);
             });
+
+            
              
             var app = builder.Build();
+
+            
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+
+                using(var scope = app.Services.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetService<AppDbContext>();
+                    dbContext.Database.EnsureCreated();
+                }
             }
 
             app.UseHttpsRedirection();
---
+++ fox-web-api/UserController.cs
@@ -0,0 +1,50 @@
+﻿using fox_web_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace fox_web_api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserController(
+            AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<User>>> Get() => await _appDbContext.Users.ToListAsync();
+
+        [HttpGet("{email}/{password}")]
+        public async Task<ActionResult<User>> Login(string email, string password)
+        {
+            if (email is not null || password is not null)
+            {
+                User user = await _appDbContext.Users.Where(user => user.Email.ToLower().Equals(email.ToLower()) && 
+                    user.Password.Equals(password))
+                        .FirstOrDefaultAsync();
+
+
+                return user != null ? Ok(user) : NotFound(user);            
+            }
+
+            return BadRequest();        
+        }
+        
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Add(User user){
+            if (user == null)
+                return BadRequest("Invalid Request");
+            var result = _appDbContext.Add<User>(user).Entity;
+            await _appDbContext.SaveChangesAsync();
+            return Ok(result);
+        }
+
+    }
+}
